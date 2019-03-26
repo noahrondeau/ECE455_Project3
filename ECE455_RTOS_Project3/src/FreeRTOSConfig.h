e474614e84052c2e8bc39cd1ee548881b006f7a5
Noah Rondeau <ntrondeau@gmail.com>
2019-03-26 09:56:22 -0700

    Change timer queue length to 20

--- ECE455_RTOS_Project3/src/FreeRTOSConfig.h
+++ ECE455_RTOS_Project3/src/FreeRTOSConfig.h
@@ -120,7 +120,7 @@ extern uint32_t SystemCoreClock;
 /* Software timer definitions. */
 #define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH		(10)
+#define configTIMER_QUEUE_LENGTH		(20)
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
